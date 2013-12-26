22aaafa23b92793e2c2125d572acc69c6bd4ea79
Rodrigo Oliveira <rodrigo@Rodrigos-MacBook-Pro.local>
2013-12-26 11:30:14 -0200

    Example spec

--- build.sbt
+++ build.sbt
@@ -5,3 +5,8 @@ version := "1.0"
 resolvers += "twttr" at "http://maven.twttr.com"
 
 libraryDependencies += "com.twitter" % "finatra" % "1.1.0"
+
+libraryDependencies += "org.specs2" %% "specs2" % "2.3.7" % "test"
+
+scalacOptions in Test ++= Seq("-Yrangepos")
+
---
+++ src/test/scala/HelloWorldSpec.scala
@@ -0,0 +1,22 @@
+package scala
+
+import org.specs2.mutable.Specification
+
+
+/**
+ * Created by rodrigo on 12/26/13.
+ */
+class HelloWorldSpec extends Specification {
+
+  "The 'Hello world' string" should {
+    "contain 11 characters" in {
+      "Hello world" must have size(11)
+    }
+    "start with 'Hello'" in {
+      "Hello world" must startWith("Hello")
+    }
+    "end with 'world'" in {
+      "Hello world" must endWith("world")
+    }
+  }
+}
